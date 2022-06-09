1bcc3a1da3999c9640350146725d9fae966eb447
Alex Silva <alexcssilva@gmail.com>
2022-06-09 17:35:17 -0300

    feat: added products.controller - Desafio 01

---
+++ src/models/product.model.ts
@@ -0,0 +1,17 @@
+import { Pool } from 'mysql2/promise';
+import Product from '../interfaces/product.interface';
+
+export default class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const result = await this.connection
+      .execute('SELECT * FROM Trybesmith.Products');
+    const [rows] = result;
+    return rows as Product[];
+  }
+}
