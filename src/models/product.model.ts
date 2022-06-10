b25017695fb4399f6ea6b9e217e36791ccde891c
Alex Silva <alexcssilva@gmail.com>
2022-06-09 22:44:54 -0300

    feat: added create products.model - Desafio 02

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import Product from '../interfaces/product.interface';
 
 export default class ProductModel {
@@ -10,8 +10,20 @@ export default class ProductModel {
 
   public async getAll(): Promise<Product[]> {
     const result = await this.connection
-      .execute('SELECT * FROM Trybesmith.Products');
+      .execute('SELECT * FROM Trybesmith.Products;');
     const [rows] = result;
     return rows as Product[];
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const result = await this.connection
+      .execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
   }
 }
