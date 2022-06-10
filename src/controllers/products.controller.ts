ec16c1d1642955942756c48095d890974eb770ed
Alex Silva <alexcssilva@gmail.com>
2022-06-09 22:43:42 -0300

    feat: added create  products.controller - Desafio 02

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -9,6 +9,14 @@ class ProductController {
     const products = await this.productService.getAll();
     res.status(StatusCodes.OK).json(products);
   };
+
+  public create = async (req: Request, res: Response) => {
+    const product = req.body;
+
+    const productCreated = await this.productService.create(product);
+    
+    return res.status(StatusCodes.CREATED).json(productCreated);
+  };
 }
 
 export default ProductController;
