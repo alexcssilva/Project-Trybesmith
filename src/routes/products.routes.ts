e16029cda95863b212ae678e78eb95b0969d7ccd
Alex Silva <alexcssilva@gmail.com>
2022-06-09 22:42:56 -0300

    feat: added .post/ product.routes - Desafio 02

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import ProductController from '../controllers/products.controller';
+import validateProduct from '../middlewares/product.middleware';
 
 const router = Router();
 
 const productsController = new ProductController();
 
 router.get('/products', productsController.getAll);
+router.post(
+  '/products', 
+  validateProduct.validateName,
+  validateProduct.validateAmount,
+  productsController.create,
+);
 
 export default router;
