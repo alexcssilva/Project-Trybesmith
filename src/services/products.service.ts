a3b49aaba736e81050e8c18113e1a96306d0fd3d
Alex Silva <alexcssilva@gmail.com>
2022-06-09 22:44:18 -0300

    feat: added create products.service - Desafio 02

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -13,6 +13,10 @@ class ProductService {
     const products = await this.model.getAll();
     return products;
   }
+
+  public async create(product: Product): Promise<Product> {
+    return this.model.create(product);
+  }
 }
 
 export default ProductService;
