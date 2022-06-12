c011cdc065eeb6214877145ca07564b01a9ef54a
Alex Silva <alexcssilva@gmail.com>
2022-06-12 20:26:31 -0300

    feat: Desafio 04 concluido

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import ProductRoutes from './routes/products.routes';
 import UserRoutes from './routes/users.routes';
+import OrderRoutes from './routes/orders.routes';
 
 const app = express();
 
 app.use(express.json());
-app.use(ProductRoutes);
 
+app.use(ProductRoutes);
 app.use(UserRoutes);
+app.use(OrderRoutes);
 
 export default app;
